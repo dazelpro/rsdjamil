217035c70b7281a3cf328694f77f31331a918a01
dazelpro <dazelpro@gmail.com>
2021-07-18 20:53:57 +0700

    + login and assets template

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,5 +50,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['default_controller'] = 'home';
+
+// Login
+$route['login'] = 'authorization';
+
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
---
+++ application/views/v-login.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RSUP Dr. M. Djamil Padang</title>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="<?php echo base_url().'assets/'?>/css/bootstrap.css">
+    <link rel="stylesheet" href="<?php echo base_url().'assets/'?>/vendors/bootstrap-icons/bootstrap-icons.css">
+    <link rel="stylesheet" href="<?php echo base_url().'assets/'?>/css/app.css">
+    <link rel="stylesheet" href="<?php echo base_url().'assets/'?>/css/pages/auth.css">
+</head>
+
+<body>
+    <div id="auth">
+        <div class="row h-100">
+            <div class="col-lg-5 col-12">
+                <div id="auth-left">
+                    <div class="auth-logo">
+                        <a href=""><img src="<?php echo base_url().'assets/'?>/images/logo/logo.png" alt="Logo"></a>
+                    </div>
+                    <h1 class="auth-title">Log in.</h1>
+                    <form action="<?php echo site_url('authorization/auth');?>">
+                        <div class="form-group position-relative has-icon-left mb-4">
+                            <input type="text" class="form-control form-control-xl" placeholder="Username or Email">
+                            <div class="form-control-icon">
+                                <i class="bi bi-person"></i>
+                            </div>
+                        </div>
+                        <div class="form-group position-relative has-icon-left mb-4">
+                            <input type="password" class="form-control form-control-xl" placeholder="Password">
+                            <div class="form-control-icon">
+                                <i class="bi bi-shield-lock"></i>
+                            </div>
+                        </div>
+                        <button class="btn btn-primary btn-block btn-lg shadow-lg mt-5">Log in</button>
+                    </form>
+                </div>
+            </div>
+            <div class="col-lg-7 d-none d-lg-block">
+                <div id="auth-right"></div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
