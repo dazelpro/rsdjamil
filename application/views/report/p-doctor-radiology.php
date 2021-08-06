2d15c675439b1830898efa2fe06226ef60d03440
dazelpro <dazelpro@gmail.com>
2021-08-07 01:27:47 +0700

    change report doctor radiology and radiographer

--- application/views/report/p-doctor-radiology.php
+++ application/views/report/p-doctor-radiology.php
@@ -12,7 +12,8 @@
 <img style="max-width: 600px; margin-top: 50px; border-bottom: 1px solid black;" src="<?php echo base_url().'assets'?>/images/kop-laporan.png" alt="">
     
     <h3>Laporan Kinerja Pelayanan Dokter Radiologi</h3>
-    <table style="width: 100%;" border="1" id="tables">
+    <br>
+    <!-- <table style="width: 100%;" border="1" id="tables">
         <thead>
             <tr>
                 <th>Pelayanan</th>
@@ -42,7 +43,52 @@
             </tr>
             <?php endforeach; ?>
         </tbody>
-    </table>
+    </table> -->
+    <?php
+        $query = $this->db->query("SELECT 
+                table_doctor_radiology.`name`, table_doctor_radiology.`id` 
+                FROM table_radiological_image 
+                JOIN table_patient ON table_patient.`mr_number` = table_radiological_image.`mr_number`
+                JOIN table_doctor_radiology ON table_doctor_radiology.`id` = table_patient.`radiology_doctor`
+                JOIN table_radiology_reading ON table_radiology_reading.`radiology` = table_radiological_image.`id`
+            GROUP BY table_doctor_radiology.name
+            ORDER BY table_doctor_radiology.id
+        ");
+        foreach ($query->result() as $doctor) :
+    ?>
+    <div style="padding-bottom: 30px">
+        <h3 style="text-align: left;"><?php echo $doctor->name;?>:</h3>
+        <table width="100%">
+            <tr style="font-weight: bold;">
+                <th width="50%">
+                    Tindakan
+                </th>
+                <th>
+                    Jumlah Pelayanan
+                </th>
+            </tr>
+            <?php
+                $id = $doctor->id;
+                $query = $this->db->query("SELECT
+                    table_handling.name, 
+                    COUNT(table_patient.mr_number) AS qty
+                FROM table_radiological_image 
+                    JOIN table_handling ON table_radiological_image.`handling` = table_handling.`id`
+                    JOIN table_patient ON table_patient.`mr_number` = table_radiological_image.`mr_number`
+                    JOIN table_doctor_radiology ON table_doctor_radiology.`id` = table_patient.`radiology_doctor`
+                WHERE table_doctor_radiology.id = '$id' AND table_radiological_image.`status` = 1
+                GROUP BY table_handling.name");
+                foreach ($query->result() as $handling) :
+            ?>
+            <tr>
+                <td style="font-weight: bold;"><?php echo $handling->name;?></td>
+                <td><?php echo $handling->qty;?></td>
+            </tr>
+            <?php endforeach;?>
+        </table>
+        
+    </div>
+    <?php endforeach;?>
 </body>
 <style>
     body {
@@ -51,33 +97,18 @@
         margin: auto;
         font-family: lato;
     }
-
-    #tables {
-        border-collapse: collapse;
-        width: 100%;
-    }
-
-    #tables td,
-    #tables th {
+    table td,
+    table th {
         border: 1px solid #ddd;
         padding: 8px;
     }
-
-    #tables tr:nth-child(even) {
-        background-color: #f2f2f2;
-    }
-
-    #tables tr:hover {
-        background-color: #ddd;
-    }
-
-    #tables th {
+    table th {
         padding-top: 12px;
         padding-bottom: 12px;
         text-align: center;
         background-color: #04AA6D;
         color: white;
-    }
+    } 
 </style>
 
 </html>
--- application/views/report/p-service-radiographer.php
+++ application/views/report/p-service-radiographer.php
@@ -12,37 +12,45 @@
     <img style="max-width: 600px; margin-top: 50px; border-bottom: 1px solid black;" src="<?php echo base_url().'assets'?>/images/kop-laporan.png" alt="">
     
     <h3>Laporan Kinerja Pelayanan Radiographer</h3>
-    <table style="width: 100%;" border="1" id="tables">
-        <thead>
-            <tr>
-                <th>Pelayanan</th>
-                <th>Jumlah Pelayanan</th>
-                <th>Admin Radiologi</th>
-                <th>Dokter Pembaca</th>
+    <br>
+    <?php
+        $query = $this->db->query("SELECT table_admin.`id`, table_admin.`name` FROM table_admin JOIN table_radiological_image ON table_radiological_image.`admin` = table_admin.`id` GROUP BY table_admin.`name` ORDER BY table_admin.`id`");
+        foreach ($query->result() as $admin) :
+    ?>
+    <div style="padding-bottom: 30px">
+        <h3 style="text-align: left;"><?php echo $admin->name;?>:</h3>
+        <table width="100%">
+            <tr style="font-weight: bold;">
+                <th width="50%">
+                    Tindakan
+                </th>
+                <th>
+                    Jumlah Pelayanan
+                </th>
             </tr>
-        </thead>
-        <tbody>
-            <?php 
-                foreach ($dataRoom->result() as $row):
+            <?php
+                $id = $admin->id;
+                $query = $this->db->query("SELECT
+                                table_handling.name, 
+                                COUNT(table_patient.mr_number) AS qty
+                    FROM table_radiological_image 
+                                JOIN table_handling ON table_radiological_image.`handling` = table_handling.`id`
+                                JOIN table_patient ON table_patient.`mr_number` = table_radiological_image.`mr_number`
+                                JOIN table_admin ON table_admin.`id` = table_radiological_image.`admin`
+                    WHERE table_admin.id = '$id'
+                    GROUP BY table_handling.name");
+                foreach ($query->result() as $handling) :
             ?>
             <tr>
-                <td>
-                    Pasien
-                    <?php echo $row->name;?>
-                </td>
-                <td>
-                    <?php echo $row->qty;?>
-                </td>
-                <td>
-                    <?php echo $row->name_admin;?>
-                </td>
-                <td>
-                    <?php echo $row->name_rad;?>
-                </td>
+                <td style="font-weight: bold;"><?php echo $handling->name;?></td>
+                <td><?php echo $handling->qty;?></td>
             </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
+            <?php endforeach;?>
+        </table>
+        
+    </div>
+    <?php endforeach;?>
+
 </body>
 <style>
     body {
@@ -51,8 +59,20 @@
         margin: auto;
         font-family: lato;
     }
+    table td,
+    table th {
+        border: 1px solid #ddd;
+        padding: 8px;
+    }
+    table th {
+        padding-top: 12px;
+        padding-bottom: 12px;
+        text-align: center;
+        background-color: #04AA6D;
+        color: white;
+    } 
 
-    #tables {
+    /* #tables {
         border-collapse: collapse;
         width: 100%;
     }
@@ -77,7 +97,7 @@
         text-align: center;
         background-color: #04AA6D;
         color: white;
-    }
+    } */
 </style>
 
 </html>
